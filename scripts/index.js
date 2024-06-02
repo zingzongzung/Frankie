a9403fc79c666ad25b325ee41165061d17d101b0
zingzongzung <the.zingzongzung@gmail.com>
2024-06-02 21:24:42 +1000

    API key added to environment variables

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
-const { generateOffchainSecretsFile } = require("./functions/gen-offchain-data.js");
+const {
+  generateOffchainSecretsFile,
+} = require("./functions/gen-offchain-data.js");
+require("@chainlink/env-enc").config();
 
 //secrets + secrets url
 generateOffchainSecretsFile(
-	{ apiKey: "a0226a78-b18b-47d3-b144-4f880080868e" },
-	"https://personal-ixqe4210.outsystemscloud.com/NFTMaker_API/offchain-secrets.json"
+  { apiKey: process.env.API_KEY },
+  "https://personal-ixqe4210.outsystemscloud.com/NFTMaker_API/offchain-secrets.json"
 ).catch((e) => {
-	console.error(e);
-	process.exit(1);
+  console.error(e);
+  process.exit(1);
 });
